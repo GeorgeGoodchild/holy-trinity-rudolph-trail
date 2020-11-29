2947b9545d278aadac9f569ec599e263ce3cee2d
George Goodchild <georgegoodchild@gmail.com>
2020-11-29 14:02:10 +0000

    testing

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 import Locations from '../views/Locations.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
   {
     path: '/locations/:locationNum',
     name: 'Locations',
---
+++ src/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    george
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
