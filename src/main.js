0c540b8f58f7796aba7f9702c5dbaf76298d39ae
George Goodchild <georgegoodchild@gmail.com>
2020-11-29 13:54:38 +0000

    meh

--- src/main.js
+++ src/main.js
@@ -4,16 +4,7 @@ import router from './router'
 
 Vue.config.productionTip = false
 
-var app = new Vue({
+new Vue({
   router,
-  render: h => h(App),
-  created () {
-    if (sessionStorage.redirect) {
-      const redirect = sessionStorage.redirect
-      delete sessionStorage.redirect
-      this.$router.push(redirect)
-    }
-  }
-})
-
-app.$mount('#app')
+  render: h => h(App)
+}).$mount('#app')
